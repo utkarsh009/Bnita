69a4819fe38542f95da09a0122e961a9580add84
Utkarsh Anand <uanand009@gmail.com>
2017-05-29 14:55:56 +0530

    Added support for running tests.
    Also, corrected the way vmm_args is constructed and passed to start_gxemul.

--- bnita.py
+++ bnita.py
@@ -10,15 +10,20 @@ class Bnita(Anita):
         self.already_installed = already_installed
     def gxemul_cdrom_args(self):
         return self.dist.iso_path()
-    vmm_args = ["-d", gxemul_cdrom_args()]
+    def gxemul_disk_args(self, path):
+        return ["-d", path]
+    vmm_args = []
+    if self.already_installed is 0:
+        vmm_args = ["-d", gxemul_cdrom_args()]
     if dist.arch() == 'pmax':
         vmm_args += ["-E 3max"]
         cd_device = 'cd0a'
     def start_gxemul(self, vmm_args, already_installed = 0):
-        child = self.pexpect_spawn(gxemul, ["-M", str(self.memory_size()), "-d", os.path.abspath(self.wd0_path), os.path.abspath(os.path.join(self.dist.download_local_arch_dir(), "binary", "kernel", ("netbsd-INSTALL.gz", "netbsd-GENERIC.gz")[already_installed]))] + (vmm_args, [])[already_installed] + self.extra_vmm_args)
+        child = self.pexpect_spawn(gxemul, ["-M", str(self.memory_size()), "-d", os.path.abspath(self.wd0_path), os.path.abspath(os.path.join(self.dist.download_local_arch_dir(), "binary", "kernel", ("netbsd-INSTALL.gz", "netbsd-GENERIC.gz")[already_installed]))] + vmm_args + self.extra_vmm_args)
         self.configure_child(child)
         return child
     if self.vmm == 'gxemul':
+        scratch_disk_args = self.gxemul_disk_args(os.path.abspath(scratch_disk_path))
         child = self.start_gxemul(vmm_args, already_installed)
     def start_boot(self, vmm_args = None):
         if vmm_args is None:
